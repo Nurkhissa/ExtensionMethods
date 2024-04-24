373909c07286a1af0361939f84e0bd0b30e0704f
Nurkhissa <45072957+Nurkhissa@users.noreply.github.com>
2024-04-24 21:55:36 +0500

    Add project files.

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,12 @@
+﻿
+string s = "Salomlar ";
+string str = s.Say("hi");
+Console.WriteLine(str);
+public static class StringExtension
+{
+    public static string Say(this string str, string message)
+    {
+
+        return str + message;
+    }
+}
